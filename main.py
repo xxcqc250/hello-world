661335ba49b982c09e6117e2e0b82d5f446c83de
xxcqc250 <a0963272507@gmail.com>
2017-08-01 12:26:36 +0800

    新增py檔案

---
+++ main.py
@@ -0,0 +1,13 @@
+# import class_1
+
+# ob=class_1.class_1("Tim",23)
+# ob.show_infomation()
+
+arr = [1,232,35,5,65,65,6]
+def sum(a):
+    ans = 0
+    for i in a:
+        ans += i
+    return ans
+
+print(sum(arr))
