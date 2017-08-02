a0f0a7c5ed42921b6869dd1bd2b43b6589f0a1af
xxcqc250 <a0963272507@gmail.com>
2017-08-02 12:47:50 +0800

    test branch2

---
+++ master1.py
@@ -0,0 +1,15 @@
+# import class_1
+
+# ob=class_1.class_1("Tim",23)
+# ob.show_infomation()
+
+
+haaaaaaaa 
+arr = [1,232,35,5,65,65,6]
+def sum(a):
+    ans = 0
+    for i in a:
+        ans += i
+    return ans
+
+print(sum(arr))
